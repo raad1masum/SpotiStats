f660faa4c984c020dbd6a7813ddf1afc03f1cdd4
rpeddakama <rishipeddakama@gmail.com>
2021-09-17 23:47:19 -0700

    stuff

--- src/main/java/com/example/sping_portfolio/controllers/Greet.java
+++ src/main/java/com/example/sping_portfolio/controllers/Greet.java
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Greet {
     @GetMapping("/greet")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, @RequestParam(required=false,
+    public String greeting(@RequestParam(required=false, defaultValue="World") String name, @RequestParam(required=false,
             defaultValue="1") String number, Model model) {
         // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
         double outNumber = Double.parseDouble(number);
-        System.out.println("number 1: " + outNumber);
+        System.out.println("number 1: " + outNumber + "name: " + name);
         outNumber = Math.pow(outNumber, 2);
         System.out.println("number 2: " + outNumber);
         model.addAttribute("name", name); // MODEL is passed to html

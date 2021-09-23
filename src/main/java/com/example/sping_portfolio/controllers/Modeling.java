f8ca1177a1b6ecc28833d00e5486f8889cb6f6e8
rpeddakama <rishipeddakama@gmail.com>
2021-09-22 17:53:39 -0700

    moving stuff from folders, commenting unecessary item in nav

--- src/main/java/com/example/sping_portfolio/controllers/Modeling.java
+++ src/main/java/com/example/sping_portfolio/controllers/Modeling.java
@@ -3,10 +3,12 @@ package com.example.sping_portfolio.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class Modeling {
     @GetMapping("/modeling")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String modeling() {
+    public String  modeling() {
         return "modeling"; // returns HTML VIEW (greeting)
     }
 }
+

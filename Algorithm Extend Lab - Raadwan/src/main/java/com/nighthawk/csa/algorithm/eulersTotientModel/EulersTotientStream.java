e382ebe22c7a1a22b8110e6a9f593b3ac3d2128d
Raadwan Masum <piraadwan@gmail.com>
2021-10-01 18:36:31 -0700

    calculate Euler's Totient using java streams

--- Algorithm Extend Lab - Raadwan/src/main/java/com/nighthawk/csa/algorithm/eulersTotientModel/EulersTotientStream.java
+++ Algorithm Extend Lab - Raadwan/src/main/java/com/nighthawk/csa/algorithm/eulersTotientModel/EulersTotientStream.java
@@ -1,6 +1,7 @@
 package com.nighthawk.csa.algorithm.eulersTotientModel;
 
-import java.util.stream.Stream;
+// import java.util.stream.Stream;
+import java.util.ArrayList;
 
 public class EulersTotientStream extends _EulersTotient {
     public EulersTotientStream() {
@@ -10,18 +11,28 @@ public class EulersTotientStream extends _EulersTotient {
         super(nth);
     }
 
+    public long gcd(long a, long b) {
+        if (b == 0) return a;
+        return gcd(b , a % b);
+    }
+
     /*
     Abstract Polymorphic "init()" method using Stream and Lambda expressions
     */
     @Override
     protected void init() {
         super.name = "Stream";
-        // Stream iterates using lambda "->" syntax unil ".limit" is reached
-        // Streams and Lambda have been added to more recent versions of Java, this will NOT be on AP Test
-        // Streams are prevalent in Big Data, in this example it seems to perform the worst
-        Stream.iterate(new long[]{0, 1}, f -> new long[]{f[1], f[0] + f[1]})
+        ArrayList<Integer> enums = new ArrayList<>();
+
+        // place all candidates into enums array
+        for (int i = 0; i < super.size; ++i) enums.add(i);
+
+        // stream to calculate phi
+        enums.stream()
             .limit(super.size)
-            .forEach(f -> super.setData(f[0]) );
+            .forEach(i -> {
+                if (gcd(i, super.size) == 1) super.setData(i);
+            });
     }
 
     /*

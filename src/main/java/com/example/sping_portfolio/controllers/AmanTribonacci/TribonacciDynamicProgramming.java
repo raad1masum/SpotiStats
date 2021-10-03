0b8def466cbcae67f9e95639dcdb511517a6e339
Aman <manmang31@gmail.com>
2021-10-03 11:55:43 -0700

    Work On 4 Algorithms Project

--- src/main/java/com/example/sping_portfolio/controllers/AmanTribonacci/TribonacciDynamicProgramming.java
+++ src/main/java/com/example/sping_portfolio/controllers/AmanTribonacci/TribonacciDynamicProgramming.java
@@ -1,4 +1,33 @@
 package com.example.sping_portfolio.controllers.AmanTribonacci;
 
-public class TribonacciDynamicProgramming {
+public class TribonacciDynamicProgramming extends TribonacciAbstract {
+    public TribonacciDynamicProgramming(String nth){
+        super(nth);
+        this.method = "dynamic programming";
+    }
+
+    private static int dynamicTribonacciNth(int n) {
+        if (n == 0 || n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        } else {
+            int tribs[] = new int[n + 2];
+            tribs[0] = 0;
+            tribs[1] = 0;
+            tribs[2] = 1;
+            for (int i = 3; i<n+1; i++) {
+                tribs[i] = tribs[i - 1] + tribs[i - 2] + tribs[i - 3];
+            }
+            return tribs[n];
+        }
+
+    }
+
+    public void compute(){
+        long startTime = System.currentTimeMillis();
+        this.value = Integer.toString(dynamicTribonacciNth(nth));
+        long endTime = System.currentTimeMillis();
+        this.time = Long.toString(endTime-startTime);
+    }
 }
--- src/main/java/com/example/sping_portfolio/controllers/AmanTribonacci/TribonacciRecursion.java
+++ src/main/java/com/example/sping_portfolio/controllers/AmanTribonacci/TribonacciRecursion.java
@@ -1,30 +1,25 @@
 package com.example.sping_portfolio.controllers.AmanTribonacci;
 
-import java.util.ArrayList;
-
 public class TribonacciRecursion extends TribonacciAbstract {
-
-    public TribonacciRecursion(String nth) {
+    public TribonacciRecursion(String nth){
         super(nth);
         this.method = "recursion";
     }
 
-
-    this.value = Integer.toString(a);
-    long endTime = System.currentTimeMillis();
-    this.time = Long.toString(endTime-startTime)
-
-    public int compute(int k) {
-        long startTime = System.currentTimeMillis();
-        k = nth
-        if (k == 0 || k == 1) {
-            return 0
-        } else if (k == 2) {
-            return 1
+    private static int tribonacciNth(int n) {
+        if (n == 0 || n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
         } else {
-            return compute(k - 1) + compute(k - 2);
+            return tribonacciNth(n - 1) + tribonacciNth(n - 2) + tribonacciNth(n - 3);
         }
+    }
+
+    public void compute(){
+        long startTime = System.currentTimeMillis();
+        this.value = Integer.toString(tribonacciNth(nth));
         long endTime = System.currentTimeMillis();
-        this.time = Long.toString(endTime - startTime);
+        this.time = Long.toString(endTime-startTime);
     }
 }

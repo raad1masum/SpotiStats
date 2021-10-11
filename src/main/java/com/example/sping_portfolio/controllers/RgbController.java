1e13ba91f1a01b9effa870dbbe7a46dbaf17f697
Rishi Peddakama <rishipeddakama@gmail.com>
2021-10-11 09:41:23 -0700

    modeling boilerplate

---
+++ src/main/java/com/example/sping_portfolio/controllers/RgbController.java
@@ -0,0 +1,36 @@
+package com.example.sping_portfolio.controllers;
+/* MVC code that shows defining a simple Model, calling View, and this file serving as Controller
+ * Web Content with Spring MVCSpring Example: https://spring.io/guides/gs/serving-web-con
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.example.sping_portfolio.controllers.ImageInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RgbController {
+    @GetMapping("/image")
+    public String image(Model model)  {
+        //String web_server = "http://localhost:8080/";
+        String web_server = "https://csa.nighthawkcodingsociety.com";
+        List<ImageInfo> lii = new ArrayList<>();
+
+        String file0 = "/images/Mona_Lisa.png";
+        lii.add(new ImageInfo(file0, web_server+file0, 12));
+        lii.get(0).read_image();
+
+        String file1 = "/images/bulb_on.gif";
+        lii.add(new ImageInfo(file1, web_server+file1, 2));
+        lii.get(1).read_image();
+
+        String file2 = "/images/bulb_off.png";
+        lii.add(new ImageInfo(file2, web_server+file2, 7));
+        lii.get(2).read_image();
+
+        model.addAttribute("lii", lii);
+        return "starters/image";
+    }
+}

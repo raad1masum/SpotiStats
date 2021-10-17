e52ee696c932f6a6a8e8567e872e4ba7204f6af0
rpeddakama <rishipeddakama@gmail.com>
2021-10-17 12:33:33 -0700

    manage controller inputs from html

--- src/main/java/com/example/sping_portfolio/controllers/AsciiLab/AsciiController.java
+++ src/main/java/com/example/sping_portfolio/controllers/AsciiLab/AsciiController.java
@@ -5,15 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.sping_portfolio.controllers.AsciiLab.ASCIIArt;
-
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class AsciiController {
     @GetMapping("/asciiLab")
     // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String asciiLab(Model model) {
-        char AsciiArray[][] = ASCIIArt.create(5, 5);
+    public String asciiLab(@RequestParam(required=false, defaultValue="10") String rowFactor, @RequestParam(required=false,
+    defaultValue="5") String colFactor, Model model) {
 
+        char AsciiArray[][] = ASCIIArt.create(Integer.parseInt(rowFactor), Integer.parseInt(colFactor));
         model.addAttribute("imageArr", AsciiArray);
         
         return "asciiLab"; // returns HTML VIEW (greeting)

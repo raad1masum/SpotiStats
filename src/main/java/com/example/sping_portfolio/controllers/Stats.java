782937beb28616633bd15704c815c11218afcf13
Raadwan Masum <piraadwan@gmail.com>
2021-10-18 00:36:07 -0700

    create method to get a token

--- src/main/java/com/example/sping_portfolio/controllers/Stats.java
+++ src/main/java/com/example/sping_portfolio/controllers/Stats.java
@@ -8,19 +8,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.*;
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
+
+import org.json.JSONObject;
 
 @Controller
 public class Stats {
-    public String getInfo() throws Exception {
+    public String getToken() throws Exception {
+        URLConnection connection = new URL("https://accounts.spotify.com/api/token").openConnection();
+
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Accept-Charset", "UTF-8");
+        connection.setRequestProperty("Authorization", "Basic NzVhOGQzOTU3NmMyNDRhYmIzYmM1OWJiY2VhZTNiNzY6MjgxOWQwMThkNmUxNGEwNGIyMTJjODQxOGI0YTBlZjI=");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + "UTF-8");
+
+        try (OutputStream output = connection.getOutputStream()) {
+            output.write("grant_type=client_credentials".getBytes(StandardCharsets.UTF_8));
+        }
+
+        InputStream response = connection.getInputStream();
+        String result = new BufferedReader(new InputStreamReader(response)) .lines().collect(Collectors.joining("\n"));
+
+        JSONObject obj = new JSONObject(result);
+        return obj.getString("access_token");
+    }
+
+    public String getInfo(String token) throws Exception {
         URL url = new URL("https://api.spotify.com/v1/playlists/5mmiPNxhTe9lS66KjakoXP");
 
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
         connection.setDoOutput(true);
         connection.setRequestProperty("Accept-Charset", "UTF-8");
-        connection.setRequestProperty("Authorization", "Bearer " + "BQDhJoWu1KpU6JPV4F3cioARklRb4rUpgeflnLErL0eFZ7GSKKxBT_LeE2boVH9BXoDGrpGRtVrkRPSafeTrB3JjQQ4grHhytEXBjzLa6FyF6GJOig_TwOnUr6efaizXk2s-LsIqnSLnASJ96L25VDXTY-IFfKjRv7wxa6obi0i1lTS-4UPczQhjlIHiEskSicfjijEF6C_2rJpgWs4NkMCIW8-S2XS4vj1f-tWUAfaF6KG_wKcNDMah9eeWtl2JcN2hysdq50LCmUSImuF17eazaDkuH1tfMg5Dc_g0hz1x");
+        connection.setRequestProperty("Authorization", "Bearer " + token);
 
         InputStream response = connection.getInputStream();
         String result = new BufferedReader(new InputStreamReader(response)) .lines().collect(Collectors.joining("\n"));
@@ -31,7 +54,7 @@ public class Stats {
     @GetMapping("/stats")
     public String stats(@RequestParam(required=false, defaultValue="") String link, Model model) throws Exception {
 
-        System.out.println(getInfo());
+        System.out.println(getInfo(getToken()));
 
         model.addAttribute("link", link);
         return "stats";

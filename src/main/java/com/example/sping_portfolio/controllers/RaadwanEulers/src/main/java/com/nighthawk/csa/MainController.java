e30ebfa1f9ffa00f13aec9daabe52f52cebc8040
Raadwan Masum <piraadwan@gmail.com>
2021-10-17 18:52:04 -0700

    remove duplicated GET for greet

--- src/main/java/com/example/sping_portfolio/controllers/RaadwanEulers/src/main/java/com/nighthawk/csa/MainController.java
+++ src/main/java/com/example/sping_portfolio/controllers/RaadwanEulers/src/main/java/com/nighthawk/csa/MainController.java
@@ -3,17 +3,17 @@ package com.example.sping_portfolio.controllers.RaadwanEulers.src.main.java.com.
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller  // HTTP requests are handled as a controller, using the @Controller annotation
 public class MainController {
 
-    @GetMapping("/greet")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
-        model.addAttribute("name", name);   // MODEL is passed to html
-        return "starters/greet";                     // returns HTML VIEW (greeting)
-    }
+    // @GetMapping("/greet")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings
+    // public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    //     // @RequestParam handles required and default values, name and model are class variables, model looking like JSON
+    //     model.addAttribute("name", name);   // MODEL is passed to html
+    //     return "starters/greet";                     // returns HTML VIEW (greeting)
+    // }
 
 
     @GetMapping("/binary")    // CONTROLLER handles GET request for /greeting, maps it to greeting() and does variable bindings

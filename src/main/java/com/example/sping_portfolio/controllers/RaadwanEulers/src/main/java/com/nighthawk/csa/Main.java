0e6e2f6ddc8754716d6094052ba1c4517454456e
Raadwan Masum <piraadwan@gmail.com>
2021-10-18 02:16:43 -0700

    comment out second main

--- src/main/java/com/example/sping_portfolio/controllers/RaadwanEulers/src/main/java/com/nighthawk/csa/Main.java
+++ src/main/java/com/example/sping_portfolio/controllers/RaadwanEulers/src/main/java/com/nighthawk/csa/Main.java
@@ -1,12 +1,12 @@
-package com.example.sping_portfolio.controllers.RaadwanEulers.src.main.java.com.nighthawk.csa;
+// package com.example.sping_portfolio.controllers.RaadwanEulers.src.main.java.com.nighthawk.csa;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Main {
-    public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
-    }
+// @SpringBootApplication
+// public class Main {
+//     public static void main(String[] args) {
+//         SpringApplication.run(Main.class, args);
+//     }
 
-}
+// }
